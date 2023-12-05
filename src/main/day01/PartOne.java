b54030ef97ff78d5e1dfe2fe4939e4e3813b97ad
Endless8 <edoardo.ciarrocchi@outlook.it>
2023-12-05 15:02:48 +0100

    Refactoring main class name

--- src/main/day01/PartOne.java
+++ src/main/day01/PartOne.java
@@ -7,7 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class Main {
+public class PartOne {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
         String puzzleInput = getPuzzleInput();
